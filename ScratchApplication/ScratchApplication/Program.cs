b14d3410d4e2774b105bcf2f2f47adc1cb82427e
JimLuth <Jim.Luth@Schneider-Electric.com>
2014-10-23 17:24:53 -0400

    Added hello world

--- ScratchApplication/ScratchApplication/Program.cs
+++ ScratchApplication/ScratchApplication/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ScratchApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+
         }
     }
 }
